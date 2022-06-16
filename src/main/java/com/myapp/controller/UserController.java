068c2def771a75d9974883f10a30623aa9ea0216
vikramaditya.k.gaikwad <vikramaditya.k.gaikwad@pwc.com>
2022-06-16 22:14:13 +0530

    validation in spring boot application

---
+++ src/main/java/com/myapp/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.myapp.model.User;
+
+import javax.validation.Valid;
+
+@Controller
+public class UserController {
+
+   @RequestMapping("add")
+   public String toAdd(User user) {
+       return "add";
+   }
+
+   @RequestMapping("addUser")
+   public String add(@Valid User user, BindingResult result) {
+       if (result.hasErrors()) {
+           return "add";
+       }
+       System.out.println("Save user =" + user);
+       return "success";
+   }
+}
